76954a888e2dcdcc0f401b5a048af720a3e37626
Martijn H. <martijn.hanrath@foleon.com>
2024-05-14 11:11:36 +0200

    content type text/plain

--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,7 @@ export const server = async () => {
     try {
       const response = await fetch('https://meme-api.com/gimme');
       const data = await response.json();
+      res.set('Content-Type', 'text/plain')
       res.send(data.url);
     } catch (error) {
       console.error('Error:', error);
